eefb971384562a6448bec653e2418ace651e9ee6
ONUOHA92 <onuohacollinschidubem@gmail.com>
2019-03-24 16:36:08 +0100

    testing

---
+++ script.js
@@ -0,0 +1,32 @@
+// this is how to toogle.
+var button = document.querySelector("button");
+
+
+
+button.addEventListener("click", function(){
+ document.body.classList.toggle("purple");
+});
+
+console.log("printed all that loops for it")
+var num = -10;
+ while(num <= 19){
+     console.log(num);
+     num++;
+ }
+console.log("printed all loop between 10 to 40")
+ var num = 10;
+ while(num <= 40){
+     console.log(num);
+     num+=2;
+ }
+ console.log("printed all loop between 300 to 333")
+ var num = 300;
+ while(num <= 333){
+     console.log(num);
+     num+=3
+ }
+
+
+
+
+
